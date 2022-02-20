49a1ebbc44e0f22274229912b00c10b7eac1ab40
ubuntu <ubuntu@localhost.localdomain>
2022-02-20 16:46:35 +0800

    fix

---
+++ 6.spmma-example/spmma_example.cudafe1.c
@@ -0,0 +1,4 @@
+# 1 "spmma_example.cu"
+# 74 "/usr/include/c++/9/iostream" 3
+static struct _ZNSt8ios_base4InitE _ZN43_INTERNAL_21_spmma_example_cpp1_ii_ef66dec1St8__ioinitE __attribute__((visibility("default"))) = {};
+extern void *__dso_handle __attribute__((visibility("hidden")));
