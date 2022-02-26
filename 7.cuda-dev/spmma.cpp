f842a2bb55bbdb4565d00d3ec6535f33b43a83d2
Dbettkk <394739138@qq.com>
2022-02-26 18:29:26 +0800

    fix

--- 7.cuda-dev/spmma.cpp
+++ 7.cuda-dev/spmma.cpp
@@ -21,6 +21,16 @@ using namespace std;
     }                                                                          \
 }
 
+#define CHECK_CUSPARSE(func)                                                   \
+{                                                                              \
+    cusparseStatus_t status = (func);                                          \
+    if (status != CUSPARSE_STATUS_SUCCESS) {                                   \
+        printf("CUSPARSE API failed at line %d with error: %s (%d)\n",         \
+               __LINE__, cusparseGetErrorString(status), status);              \
+        return EXIT_FAILURE;                                                   \
+    }                                                                          \
+}
+
 struct Matrix {
     __half *item;
     int row, col;
