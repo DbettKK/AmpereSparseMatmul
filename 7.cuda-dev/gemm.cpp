a582f2a6f60b3864b23499430b4cf436945d9250
Dbettkk <394739138@qq.com>
2022-03-02 17:03:36 +0800

    fix

--- 7.cuda-dev/gemm.cpp
+++ 7.cuda-dev/gemm.cpp
@@ -35,13 +35,13 @@ double **read_bin(int m, int n, int k) {
     double *b_float = new double[k * n];
     double *c_float = new double[m * n];
     for (int i = 0; i < m * k; i++) {
-        a_float[i] = static_cast<double>(mat_a_host[i]);
+        a_float[i] = static_cast<double>(static_cast<float>(mat_a_host[i]));
     }
     for (int i = 0; i < n * k; i++) {
-        b_float[i] = static_cast<double>(mat_b_host[i]);
+        b_float[i] = static_cast<double>(static_cast<float>(mat_b_host[i]));
     }
     for (int i = 0; i < m * n; i++) {
-        c_float[i] = static_cast<double>(mat_c_host[i]);
+        c_float[i] = static_cast<double>(static_cast<float>(mat_c_host[i]));
     }
     ret[0] = a_float;
     ret[1] = b_float;
