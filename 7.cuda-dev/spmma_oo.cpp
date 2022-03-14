ef017d044c18522978818ab4bb825bb301fdf1f8
Dbettkk <394739138@qq.com>
2022-03-14 10:36:41 +0800

    fix

--- 7.cuda-dev/spmma_oo.cpp
+++ 7.cuda-dev/spmma_oo.cpp
@@ -688,14 +688,14 @@ spmmaStatus_t __mma_matmul_A(MatrixParam *param, __half *matA_cmpr) {
     cout << compressed_size / sizeof(void) << endl;
     CHECK_CUSPARSE( cusparseLtSpMMACompress(&handle, &plan, dA, dA_compressed, stream) )
     __half *hA_compressed = new __half[compressed_size / sizeof(__half)];
-    CHECK_CUDA( cudaMemcpy(hA_compressed, dA_compressed, compressed_size, cudaMemcpyDeviceToHost) )
+    CHECK_CUDA( cudaMemcpyAsync(hA_compressed, dA_compressed, compressed_size, cudaMemcpyDeviceToHost, stream) )
     __half *cmpr_new = new __half[compressed_size / sizeof(__half)];
     //memset(cmpr_new, -7096, compressed_size);
     for (int i = 0; i < compressed_size / sizeof(__half); i++) {
         if (i < compressed_size / sizeof(__half) / 2) cmpr_new[i] = matA_cmpr[i];
         else {
             cmpr_new[i] = hA_compressed[i];
-            printf("%u | ", hA_compressed[i]);
+            printf("%f | ", hA_compressed[i]);
 
             cout << static_cast<__half>(hA_compressed[i]) << " ";
             cout << endl;
