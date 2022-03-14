c55146190290c6d968ec8a378391021390c1e142
Dbettkk <394739138@qq.com>
2022-03-14 10:26:44 +0800

    fix

--- 7.cuda-dev/spmma_oo.cpp
+++ 7.cuda-dev/spmma_oo.cpp
@@ -695,7 +695,7 @@ spmmaStatus_t __mma_matmul_A(MatrixParam *param, __half *matA_cmpr) {
         if (i < compressed_size / sizeof(__half) / 2) cmpr_new[i] = matA_cmpr[i];
         else {
             //cmpr_new[i] = hA_compressed[i];
-            cout << /cmpr_new[i] << " ";
+            cout << cmpr_new[i] << " ";
         }
     }
     cout << endl;
