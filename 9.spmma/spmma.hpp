c1853f2370735b63e0e3817e87b889eeb04670e0
Dbettkk <394739138@qq.com>
2022-03-18 12:36:22 +0800

    fix

--- 9.spmma/spmma.hpp
+++ 9.spmma/spmma.hpp
@@ -54,7 +54,7 @@ __global__ void im2col_gpu_kernel(const int n, const Dtype* data_im, const int d
                 int w_im = w_offset + j * dilation_w;
                 *data_col_ptr =
                     (h_im >= 0 && w_im >= 0 && h_im < height && w_im < width) ?
-                    data_im_ptr[i * dilation_h * width + j * dilation_w] : float(0);
+                    data_im_ptr[i * dilation_h * width + j * dilation_w] : static_cast<__half>(static_cast<float>(0));
                 data_col_ptr += data_n * height_col * width_col;
               }
             }
