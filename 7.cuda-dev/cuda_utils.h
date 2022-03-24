31985179d5a7a624a40b1a7a7f2f7a44158fb057
Dbettkk <394739138@qq.com>
2022-03-24 19:24:15 +0800

    fix

--- 7.cuda-dev/cuda_utils.h
+++ 7.cuda-dev/cuda_utils.h
@@ -78,7 +78,7 @@ const int k_global = kernel_c_global * kernel_w_global * kernel_h_global;
 const int n_global = kernel_n_global;
 
 //string path = "kernel_" + to_string(kernel_w_global) + "x" + to_string(kernel_w_global) + "/" + to_string(data_w_global) + "x" + to_string(data_w_global) + "/";
-string path = "";
+string path = "bin/";
 string data_path = path + "data.bin";
 string kernel_path = path + "kernel.bin";
 string a_path = path + "a.bin";
